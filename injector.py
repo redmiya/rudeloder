683e40c47c5e8d8c4a025dbde8800c8ac408b6ba
redmiya <akamiya@vulnerotona.xyz>
2023-04-05 21:23:39 +0900

    change implements

--- injector.py
+++ injector.py
@@ -1,31 +1,35 @@
 import wave
 import argparse
+import struct
 
-def embed_code_in_wav(input_wav_path, output_wav_path, encrypted_code):
+def embed_code_in_wav(input_wav_path, output_wav_path, code):
     with wave.open(input_wav_path, 'rb') as input_wav:
         params = input_wav.getparams()
         frames = input_wav.readframes(params.nframes)
-        
+
         with wave.open(output_wav_path, 'wb') as output_wav:
             output_wav.setparams(params)
-            
-            output_wav.setsampwidth(params.sampwidth)
-            output_wav.setnchannels(params.nchannels)
-            output_wav.setframerate(params.framerate)
-            output_wav.setnframes(params.nframes)
-            output_wav.setcomptype(params.comptype, encrypted_code)
-            
-            output_wav.writeframes(frames)
+
+            # Define a marker to indicate the start of the embedded code
+            marker = b'CODE'
+            end_marker = b'ENDCODE'
+
+            # Embed the code after the marker in the data chunk
+            embedded_frames = bytearray(frames)
+            embedded_code = marker + code.encode('utf-8') + end_marker
+            embedded_frames[:len(embedded_code)] = embedded_code
+
+            output_wav.writeframes(embedded_frames)
 
 def main():
-    parser = argparse.ArgumentParser(description="Embed encrypted code in a WAV file")
+    parser = argparse.ArgumentParser(description="Embed code in a WAV file")
     parser.add_argument("input_wav_path", help="Path to the input WAV file")
     parser.add_argument("output_wav_path", help="Path to the output WAV file")
-    parser.add_argument("encrypted_code", help="The encrypted code to embed")
+    parser.add_argument("code", help="The code to embed")
 
     args = parser.parse_args()
-    
-    embed_code_in_wav(args.input_wav_path, args.output_wav_path, args.encrypted_code)
+
+    embed_code_in_wav(args.input_wav_path, args.output_wav_path, args.code)
 
 if __name__ == "__main__":
     main()
--- main.rs
+++ main.rs
@@ -1,44 +1,97 @@
 extern crate hound;
 
-use hound::WavReader;
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::Read;
+use std::io::Write;
+use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("Usage: {} <path/to/your/wav/unko.wav>", args[0]);
+        println!("Usage: {} <path/to/your/wav/file.wav>", args[0]);
         return;
     }
 
     let filepath = &args[1];
-    let encrypted_code =
-        extract_encrypted_code(filepath).expect("Failed to extract encrypted code"); // Extract Encrypted Codes
+    let code = extract_encrypted_code(filepath).expect("Failed to extract encrypted code");
 
-    let decrypted_code = decrypt_code(&encrypted_code);
+    // Encrypted-Code is Decrypting here but * in this code is a Test so no encryption/decryption implemented
+    let decrypted_code = String::from_utf8(code).expect("Failed to convert code to string");
+
+    // Execute Decrypted-Code
     execute_code(&decrypted_code);
 }
 
 fn extract_encrypted_code(filepath: &str) -> std::io::Result<Vec<u8>> {
     let mut file = File::open(filepath)?;
-    let mut buffer = [0; 44]; // Read first 44 bytes of WAV header
-    file.read_exact(&mut buffer)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
 
-    let encrypted_code_start = 36; // Assuming the encrypted code is at position 36 in header
-    let encrypted_code_length = 4; // Assuming the length of the encrypted code is 4 bytes
-    let encrypted_code =
-        buffer[encrypted_code_start..encrypted_code_start + encrypted_code_length].to_vec();
+    let marker = b"CODE"; // Code Embedded WAV file including marker of like "CODE" means Starting EmbedCode
+    let end_marker = b"ENDCODE"; // Also "ENDCODE" means Ending EmbedCode
+    let marker_position = buffer
+        .windows(marker.len())
+        .position(|window| window == marker)
+        .expect("Marker not found in WAV file");
 
-    Ok(encrypted_code)
+    let end_marker_position = buffer
+        .windows(end_marker.len())
+        .position(|window| window == end_marker)
+        .expect("End marker not found in WAV file");
+
+    let code_start = marker_position + marker.len();
+    let code_end = end_marker_position;
+    let code = buffer[code_start..code_end].to_vec();
+
+    Ok(code)
 }
 
-fn decrypt_code(encrypted_code: &[u8]) -> Vec<u8> {
-    // Replace this with your decryption algorithm
-    encrypted_code.to_vec()
-}
+fn execute_code(code: &str) {
+    // Write Code for temp
+    let mut tmp_file = tempfile::Builder::new()
+        .prefix("temp_code_")
+        .suffix(".rs")
+        .tempfile()
+        .expect("Failed to create temporary file");
+    tmp_file
+        .write_all(code.as_bytes())
+        .expect("Failed to write code to temporary file");
+    let tmp_path = tmp_file.into_temp_path();
 
-fn execute_code(decrypted_code: &[u8]) {
-    // Replace this with your code execution logic
-    println!("Decrypted code: {:?}", decrypted_code);
+    // Compile the code here
+    let output = Command::new("rustc")
+        .arg(&tmp_path)
+        .arg("-o")
+        .arg("tmp_executable")
+        .output()
+        .expect("Failed to compile code");
+
+    if !output.status.success() {
+        eprintln!(
+            "Compilation error: {}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+        return;
+    }
+
+    // Execute binary written in temp
+    let output = Command::new("./tmp_executable")
+        .output()
+        .expect("Failed to execute code");
+
+    if output.status.success() {
+        println!(
+            "Code executed successfully: {}",
+            String::from_utf8_lossy(&output.stdout)
+        );
+    } else {
+        eprintln!(
+            "Execution error: {}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
+
+    // Delete temp
+    std::fs::remove_file("tmp_executable").expect("Failed to remove temporary executable");
 }
